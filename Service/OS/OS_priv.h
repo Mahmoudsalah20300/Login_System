e644acb3d3f8380da207007cbfa638db0a75038b
Mahmoudsalah20300 <Mahmoudsalah20300@gmail.com>
2024-03-31 18:13:43 +0200

    This is the Service Layer where Os can be used Later

---
+++ Service/OS/OS_priv.h
@@ -0,0 +1,19 @@
+/*
+ * OS_priv.h
+ *
+ *  Created on: Mar 5, 2023
+ *      Author: m_hsa
+ */
+
+#ifndef OS_PRIV_H_
+#define OS_PRIV_H_
+
+typedef struct
+{
+	u16 Periodicity;
+	void(*TaskFunc)(void);
+}Task_t;
+
+static void voidScheduler(void);
+
+#endif /* OS_PRIV_H_ */
